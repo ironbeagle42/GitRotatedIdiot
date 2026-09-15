1b0b60ff96eb349bfed25855318efe85dfea48c6
ironbeagle <mmaynard010@gmail.com>
2026-09-15 14:58:52 -0700

    (GP-1.12): Git.java now has init function

--- Git.java
+++ Git.java
@@ -1,5 +1,14 @@
+import java.io.File;
+
 public class Git {
     public static void main(String[] args) {
-        
+        File a = new File("git/");
+        a.mkdir();
+        File b = new File("git/objects/");
+        b.mkdir();
+        File c = new File("git/index");
+        c.mkdir();
+        File d = new File("git/HEAD");
+        d.mkdir();
     }
 }
